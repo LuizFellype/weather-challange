c664e06bd5eed9720ba0daec1ae99adc20f230f1
Luiz Fellype <lfcassago@gmail.com>
2025-04-02 20:22:26 -0300

    feat: implement city card component

--- src/components/CityCard.jsx
+++ src/components/CityCard.jsx
@@ -1,9 +1,67 @@
 import * as React from "react";
 
-const CityCard = () => {
+const WeatherInfo = ({ label, value, className = '' }) => {
   return (
-    <div />
+    <div className={`flex flex-col items-center p-1 rounded-lg ${className}`}>
+      <span className="text-xs text-gray-900">{label}</span>
+      <span className="text-base font-bold text-gray-900">{value}</span>
+    </div>
+  )
+}
+
+const blue = 'bg-sky-500/20'
+const orange = 'bg-orange-500/20'
+const red = 'bg-red-500/20'
+const getTemperatureColorCode = (temp) => {
+  const isBiggerThan5 = temp > 5
+  const isBiggerThan25 = temp > 25
+
+  if (isBiggerThan25) return red
+  if (isBiggerThan5) return orange
+
+  return blue
+}
+
+const normalizeDataToParams = ({ data: {
+  timezone,
+  current: { temp, pressure, humidity }
+} }) => ({
+  timezone,
+  temp,
+  temp: `${temp} °C`,
+  pressure,
+  humidity: `${humidity}%`,
+  tempColorCode: getTemperatureColorCode(temp)
+})
+
+const CityCard = ({ 
+  timezone,
+  temp,
+  pressure,
+  humidity,
+  tempColorCode
+}) => {
+  return (
+    <div className="m-2 items-center flex flex-col md:flex-row md:justify-center cursor-default">
+      <div className="w-60 transition duration-500 ease-in-out transform bg-white rounded-lg hover:scale-105 cursor-pointer border flex flex-col justify-center items-center text-center p-6">
+        <div className="text-md flex flex-col text-gray-900">
+          <span className="uppercase font-bold">{timezone}</span>
+        </div>
+
+        <div className="flex gap-1">
+          <WeatherInfo label="Temp" value={temp} className={tempColorCode} />
+          <WeatherInfo label="Pressure" value={pressure} />
+          <WeatherInfo label="Hum" value={humidity} />
+        </div>
+      </div>
+    </div>
   )
 };
 
-export default CityCard
+const CityCardWrapper = (props) => {
+  const data = normalizeDataToParams(props)
+
+  return <CityCard {...data} />
+};
+
+export default CityCardWrapper
---
+++ src/mock-db/weather.js
@@ -0,0 +1,93 @@
+
+export const allWeathers = [
+  {
+    "lat": 32.44,
+    "lon": -94.04,
+    "timezone": "City 1",
+    "timezone_offset": -18000,
+    "current": {
+      "dt": 1684929490,
+      "sunrise": 1684926645,
+      "sunset": 1684977332,
+      "temp": 5,
+      "feels_like": 292.87,
+      "pressure": 1014,
+      "humidity": 89,
+      "dew_point": 290.69,
+      "uvi": 0.16,
+      "clouds": 53,
+      "visibility": 10000,
+      "wind_speed": 3.13,
+      "wind_deg": 93,
+      "wind_gust": 6.71,
+      "weather": [
+        {
+          "id": 803,
+          "main": "Clouds",
+          "description": "broken clouds",
+          "icon": "04d"
+        }
+      ]
+    },
+  },
+  {
+    "lat": 33.44,
+    "lon": -94.04,
+    "timezone": "City 2",
+    "timezone_offset": -18000,
+    "current": {
+      "dt": 1684929490,
+      "sunrise": 1684926645,
+      "sunset": 1684977332,
+      "temp": 25,
+      "feels_like": 292.87,
+      "pressure": 1014,
+      "humidity": 89,
+      "dew_point": 290.69,
+      "uvi": 0.16,
+      "clouds": 53,
+      "visibility": 10000,
+      "wind_speed": 3.13,
+      "wind_deg": 93,
+      "wind_gust": 6.71,
+      "weather": [
+        {
+          "id": 803,
+          "main": "Clouds",
+          "description": "broken clouds",
+          "icon": "04d"
+        }
+      ]
+    },
+  },
+  {
+    "lat": 33.44,
+    "lon": -94.04,
+    "timezone": "City 3",
+    "timezone_offset": -18000,
+    "current": {
+      "dt": 1684929490,
+      "sunrise": 1684926645,
+      "sunset": 1684977332,
+      "temp": 26,
+      "feels_like": 292.87,
+      "pressure": 1014,
+      "humidity": 89,
+      "dew_point": 290.69,
+      "uvi": 0.16,
+      "clouds": 53,
+      "visibility": 10000,
+      "wind_speed": 3.13,
+      "wind_deg": 93,
+      "wind_gust": 6.71,
+      "weather": [
+        {
+          "id": 803,
+          "main": "Clouds",
+          "description": "broken clouds",
+          "icon": "04d"
+        }
+      ]
+    },
+  },
+];
